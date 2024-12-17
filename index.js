8806f3ad4d6d1d2f1716f98688b1db46c4af941a
Esac86 <esacevedo86@gmail.com>
2024-12-16 21:33:03 -0500

    Init 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Backend encendido.");
+    res.send("Servidor backend encendido.");
 });
 
 app.listen(port, () => {
